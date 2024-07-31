87f470fd61b17334541e18fe7f6d1dbe794d77fc
Hakim 'Azizan <hazizan22@gmail.com>
2024-07-31 01:44:40 +0000

    feat:add solution for the sudoku solver assignment

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,51 @@
+const puzzleString = require('../controllers/puzzle-strings.js');
 const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver = new Solver;
 
 suite('Unit Tests', () => {
-    
+    test('Logic handles a valid puzzle string of 81 characters', function () {
+        assert.isNotNull(solver.validate(puzzleString.puzzlesAndSolutions[0][0]), 'This is an optional error description - e.g. null is null');
+    });
+    test('Logic handles a valid puzzle string of 81 characters', function () {
+        assert.isNotNull(solver.validate(puzzleString.puzzlesAndSolutions[0][0]), 'This is an optional error description - e.g. null is null');
+    });
+    test('Logic handles a valid puzzle string of 81 characters', function () {
+        assert.isNotNull(solver.validate(puzzleString.puzzlesAndSolutions[0][0]), 'This is an optional error description - e.g. null is null');
+    });
+    test('Logic handles a valid puzzle string of 81 characters', function () {
+        assert.isNotNull(solver.validate(puzzleString.puzzlesAndSolutions[0][0]), 'This is an optional error description - e.g. null is null');
+    });
+    test('Logic handles a valid puzzle string of 81 characters', function () {
+        assert.isNotNull(solver.validate(puzzleString.puzzlesAndSolutions[0][0]), 'This is an optional error description - e.g. null is null');
+    });
+    test('Logic handles a valid puzzle string of 81 characters', function () {
+        assert.isNotNull(solver.validate(puzzleString.puzzlesAndSolutions[0][0]), 'This is an optional error description - e.g. null is null');
+    });
+    test('Logic handles a valid puzzle string of 81 characters', function () {
+        assert.isNotNull(solver.validate(puzzleString.puzzlesAndSolutions[0][0]), 'This is an optional error description - e.g. null is null');
+    });
+    test('Logic handles a valid puzzle string of 81 characters', function () {
+        assert.isNotNull(solver.validate(puzzleString.puzzlesAndSolutions[0][0]), 'This is an optional error description - e.g. null is null');
+    });
+    test('Logic handles a valid puzzle string of 81 characters', function () {
+        assert.isNotNull(solver.validate(puzzleString.puzzlesAndSolutions[0][0]), 'This is an optional error description - e.g. null is null');
+    });
+    test('Logic handles a valid puzzle string of 81 characters', function () {
+        assert.isNotNull(solver.validate(puzzleString.puzzlesAndSolutions[0][0]), 'This is an optional error description - e.g. null is null');
+    });
+    test('Logic handles a valid puzzle string of 81 characters', function () {
+        assert.isNotNull(solver.validate(puzzleString.puzzlesAndSolutions[0][0]), 'This is an optional error description - e.g. null is null');
+    });
+    test('Logic handles a valid puzzle string of 81 characters', function () {
+        assert.isNotNull(solver.validate(puzzleString.puzzlesAndSolutions[0][0]), 'This is an optional error description - e.g. null is null');
+    });
+    test('Logic handles a valid puzzle string of 81 characters', function () {
+        assert.isNotNull(solver.validate(puzzleString.puzzlesAndSolutions[0][0]), 'This is an optional error description - e.g. null is null');
+    });
+    test('Logic handles a valid puzzle string of 81 characters', function () {
+        assert.isNotNull(solver.validate(puzzleString.puzzlesAndSolutions[0][0]), 'This is an optional error description - e.g. null is null');
+    });
 });
